0b702bd2cb1d01b6d67b7e885c461588aea023ec
Felix Lin <flexautobig@gmail.com>
2024-07-06 23:52:32 +0800

    Add files via upload

---
+++ HappyZip.java
@@ -0,0 +1,44 @@
+package gadget;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.lang.reflect.Method;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class HappyZip extends JFrame implements MouseMotionListener {
+	
+	public static void main( String[] args ) {
+
+		new HappyZip();
+		
+	}
+	
+	@Override
+	public void mouseDragged( MouseEvent e ) {}
+
+	@Override
+	public void mouseMoved( MouseEvent e ) {}
+
+	private boolean testForHasMethods() {
+		
+		return ( ! hasMethods( MockA.class ) ) && ( hasMethods( MockB.class ) );
+		
+	}
+	private static class MockA {}
+	private static class MockB {
+		
+		private void forTest() {}
+		
+	}
+	private boolean hasMethods( Class<?> any ) {
+		
+		Method[] methodsHere = any.getDeclaredMethods();
+		if ( methodsHere.length > 0 )
+			return true;
+		return false;
+			
+	}
+
+}
